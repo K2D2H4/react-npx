edeed81003e5d1bcddc5914c174f96d893c69784
KDH <daehyun9714@gmail.com>
2024-05-08 09:41:22 +0900

    commit

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,30 +1,41 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 function Detail() {
-  const [loading, setLoading] = useState(true);
-  const [details, setDetail] = useState([]);
-
-  const getMovies = async () => {
-    const json = getMovie();
-    setDetail(json.data.movie);
-    setLoading(false);
-  };
-
   const { id } = useParams();
+  const [loading, setLoading] = useState(true);
+  const [details, setDetail] = useState("");
+
   const getMovie = async () => {
     const json = await (
       await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
     ).json();
-    console.log(json);
-    return json;
+    setDetail(json);
+    setLoading(false);
   };
 
   useEffect(() => {
     getMovie();
   }, []);
+
   return (
     <div>
-      {loading ? <h1>loading...</h1> : <h1>movies{console.log(details.id)}</h1>}
+      {loading ? (
+        <h1>Loading,,,</h1> // if it is still on loading,,
+      ) : (
+        // if loading finished,,,
+        <div>
+          <img src={details.data.movie.medium_cover_image}></img>
+          <h3> Title : {details.data.movie.title}</h3>
+          <h3> Genres : {details.data.movie.genres[0]}</h3>
+          <h3> Year : {details.data.movie.year}</h3>
+          <h3> Languages : {details.data.movie.language}</h3>
+          <h3>
+            <Link to="/" style={{ textDecoration: "none", color: "black" }}>
+              Go back
+            </Link>
+          </h3>
+        </div>
+      )}
     </div>
   );
 }
